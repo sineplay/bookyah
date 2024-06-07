f0b4afc71913ffe55b45ad125ba1350ab0ccbc33
GameQber <15821063+sineplay@users.noreply.github.com>
2024-06-07 16:46:29 -0400

    Fix for reservation import with series_id set to None

--- mycal/booking/resources.py
+++ mycal/booking/resources.py
@@ -1,4 +1,4 @@
-from import_export import resources
+from import_export import widgets, resources, fields
 from import_export.admin import ImportExportModelAdmin
 from .models import AssetType, Asset, Reservation
 
@@ -10,6 +10,18 @@ class AssetResource(resources.ModelResource):
 	class Meta:
 		model = Asset
 
+class NullableUUIDWidget(widgets.UUIDWidget):
+	def clean(self, value, row=None, *args, **kwargs):
+		if value in ('None', ''):
+			return None
+		return super().clean(value, row=row, *args, **kwargs)
+
 class ReservationResource(resources.ModelResource):
+    series_id = fields.Field(
+		column_name='series_id',
+		attribute='series_id',
+		widget=NullableUUIDWidget(),
+    )
+	
     class Meta:
         model = Reservation
